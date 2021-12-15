a6b8e47a3c2b7130aa9dd40cf1fe8a21836e583c
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2021-12-15 19:20:24 +0330

    Implement Domain

---
+++ Phonebook.Domain/Entities/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Domain.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Company { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public Contact()
+        {
+
+        }
+        public Contact(string firstName, string lastName, string company, string phoneNumber, string description)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Company = company;
+            PhoneNumber = phoneNumber;
+            Description = description;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Update(string firstName, string lastName, string company, string phoneNumber, string description)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Company = company;
+            PhoneNumber = phoneNumber;
+            Description = description;
+        }
+    }
+}
