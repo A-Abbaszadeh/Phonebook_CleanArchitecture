00b4fd9ad5357b24e051f6cb9e611500da9f1146
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2021-12-15 22:25:00 +0330

    Implementation Add New Contact Service

---
+++ Phonebook.Application/Interfaces/Contexts/IDatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Phonebook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Application.Interfaces.Contexts
+{
+    public interface IDatabaseContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public int SaveChanges();
+    }
+}
---
+++ Phonebook.Application/Services/AddNewContact/AddNewContactService.cs
@@ -0,0 +1,46 @@
+﻿using Phonebook.Application.Dto;
+using Phonebook.Application.Interfaces.Contexts;
+using Phonebook.Domain.Entities;
+
+namespace Phonebook.Application.Services.AddNewContact
+{
+    public class AddNewContactService : IAddNewContactService
+    {
+        private readonly IDatabaseContext _context;
+
+        public AddNewContactService(IDatabaseContext context)
+        {
+            _context = context;
+
+        }
+        public ResultDto Execute(AddNewContactDto newContact)
+        {
+            if (string.IsNullOrEmpty(newContact.PhoneNumber))
+            {
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = "شماره تلفن را وارد کنید"
+                };
+            }
+
+            Contact contact = new Contact
+                (
+                newContact.FirstName, 
+                newContact.LastName,
+                newContact.Company, 
+                newContact.PhoneNumber, 
+                newContact.Description
+                );
+
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+
+            return new ResultDto
+            {
+                IsSuccess = true,
+                Message = $"{contact.FirstName} {contact.LastName} با موفقیت ثبت گردید"
+            };
+        }
+    }
+}
